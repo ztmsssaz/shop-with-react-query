103ef0031f07ee334d52e2368f9335a5310eb546
unknown <zarei.mrabolfazl@gmail.com>
2025-02-27 11:16:43 +0330

    Install react-query & Config Provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,18 @@ import i18n from './i18.ts'
 import './index.css'
 import {Provider} from 'react-redux'
 import {store} from './store/store.ts'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+const client = new QueryClient({})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
+      <QueryClientProvider client={client}>
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 )
